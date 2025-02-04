9e94afa89aec06a8f332fee5a977d62279ff8e7f
Viktoriia Shutilova <shytilovav@ukr.net>
2025-02-04 23:34:46 +0100

    Convert fahrenheit to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,21 @@ package app;
 
 public class Main {
 
+    private static final double CONV_VAL1=32;
+    private static final double CONV_VAL2=1.8;
+
     public static void main(String[] args) {
         System.out.println("Converter unit temperature");
 
+        double deegreFahrenheit =70;
+        double deegreeCel = fahrenheitToCelsius(deegreFahrenheit);
+
+        System.out.printf("%.1f\u00B0F = %.1f\u00B0C",
+                deegreFahrenheit,
+                deegreeCel);
+    }
+
+    private static double fahrenheitToCelsius(double deegreFahrenheit){
+        return (deegreFahrenheit-CONV_VAL1)/CONV_VAL2;
     }
 }
